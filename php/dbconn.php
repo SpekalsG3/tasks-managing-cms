1ca2b7d8511677bbfafef7825d8c4223741d1904
SpekalsG3 <46575914+SpekalsG3@users.noreply.github.com>
2020-03-07 22:38:12 +0300

    Update dbconn.php

--- php/dbconn.php
+++ php/dbconn.php
@@ -19,6 +19,6 @@ try {
   $conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $username, $dbpass);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-  echo '<div class="msg msg--error"><div class="msg__title">' . $title . '</div><div class="msg__text">' . $msg .'</div></div>';
+  echo '<div class="msg msg--error"><div class="msg__title">SQL Error</div><div class="msg__text">Failed to connect to database</div></div>';
   exit();
 }
